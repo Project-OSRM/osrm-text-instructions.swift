bddab7e463018f9e743ea9326fecfecd5a24cd84
Freenerd <nospam@freenerd.de>
2016-11-07 14:13:31 +0100

    Rename to OSRMTextInstructions.swift
    Move tests to own OSRMTextInstructionsTests.swift

---
+++ VoyageTests/OSRMTextInstructionsTests.swift
@@ -0,0 +1,61 @@
+//
+//  OSRMTextInstructionsTests.swift
+//  Voyage
+//
+//  Created by Johan Uhle on 07.11.16.
+//  Copyright © 2016 Mapbox. All rights reserved.
+//
+
+import XCTest
+
+class OSRMTextInstructionsTests: XCTestCase {
+    let instructions = OSRMTextInstructions(version: "v5", language: "en")
+
+    func testFixtures() {
+        do {
+            let url = URL(fileURLWithPath: "/Users/johan/Code/mapbox/voyage/osrm-text-instructions/test/fixtures/v5/")
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [])
+            for type in directoryContents {
+                let typeDirectoryContents = try FileManager.default.contentsOfDirectory(at: type, includingPropertiesForKeys: nil, options: [])
+
+                for fixture in typeDirectoryContents {
+                    // parse fixture
+                    let json = getFixture(url: fixture) as! [ String: AnyObject ]
+                    let step = Step(json: json["step"] as! [ String: AnyObject ])
+
+                    // compile instruction
+                    let instructions = self.instructions.compile(step: step)
+
+                    // check generated instruction against fixture
+                    // TODO: Remove unnecessary type filter
+                    if (true || step.maneuver.type == "turn") {
+                        XCTAssertEqual(
+                            json["instruction"] as? String,
+                            instructions,
+                            fixture.path
+                        )
+                    }
+                }
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            XCTAssert(false)
+        }
+    }
+
+    func getFixture(url: URL) -> Any {
+        var json = Data()
+        do {
+            json = try Data(contentsOf: url, options: [])
+        } catch {
+            XCTAssert(false)
+        }
+        var routeJSON: Any!
+        do {
+            routeJSON = try JSONSerialization.jsonObject(with: json, options: [])
+        } catch {
+            XCTAssert(false)
+        }
+        return routeJSON
+    }
+}
