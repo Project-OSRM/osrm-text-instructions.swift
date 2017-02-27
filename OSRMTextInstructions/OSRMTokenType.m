bc1a8106d9dfd182f1b31d06a47a7919ee02e702
bsudekum <bobby@mapbox.com>
2017-02-27 14:16:58 -0800

    objc bridge

---
+++ OSRMTextInstructions/OSRMTokenType.m
@@ -0,0 +1,10 @@
+#import "OSRMTokenType.h"
+
+OSRMTokenType const OSRMTokenWayName = @"wayName";
+OSRMTokenType const OSRMTokenDestination = @"destination";
+OSRMTokenType const OSRMTokenRotaryName = @"rotaryName";
+OSRMTokenType const OSRMTokenExit = @"exit";
+OSRMTokenType const OSRMTokenLaneInstruction = @"laneInstruction";
+OSRMTokenType const OSRMTokenModifier = @"modifier";
+OSRMTokenType const OSRMTokenDirection = @"direction";
+OSRMTokenType const OSRMTokenWayPoint = @"wayPoint";
